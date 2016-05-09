c1cfbed1b1d0affde77c6c7d9c01f09163fddf3e
mwalercz <mwalerczuk@gmail.com>
2016-05-10 01:32:59 +0200

    more minor changes to vt_req

--- server/requesthandlers/vt_request.py
+++ server/requesthandlers/vt_request.py
@@ -7,7 +7,6 @@ from server.fileservice import write_to_file
 
 def request_to_vt(sha256):
     headers = {"Cache-Control": "no-cache",
-               "Cookie": "__utma=194538546.1374540295.1458168968.1462804416.1462835638.29; __utmz=194538546.1462534007.22.10.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); VT_CSRF=7307f46a6f03a9a38eed4f19c801187a; VT_PREFERRED_LANGUAGE=en; __utmb=194538546.2.10.1462835638; __utmc=194538546; __utmt=1",
                "Accept": "text/html",
                "Connection": "keep-alive",
                "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
@@ -16,7 +15,7 @@ def request_to_vt(sha256):
     responseVT = requests.get(url, headers=headers)
     data = responseVT.text
     empty_file = ""
-    if (data == empty_file):
+    if data == empty_file:
         logging.info("No file with sha256:" + sha256 + " on VirusTotal.")
     else:
         write_to_file(sha256, data)
