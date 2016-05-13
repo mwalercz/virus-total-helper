58030e84ba7c2fd61debd4998386207a5622602f
mborkows <mborkows@mion.elka.pw.edu.pl>
2016-05-13 02:37:51 +0200

    cycle_request

--- server/requesthandlers/cycle_request.py
+++ server/requesthandlers/cycle_request.py
@@ -5,10 +5,46 @@ import logging
 
 from server.customhttp import NotJsonError
 from server.requesthandlers import vt_request
+from server.customhttp import HTTPResponse
 
 
 def cycle_handler(request, response, scheduler):
-    # obliczamy sha256 dla pliku binarnego
+
+    if not is_json(request, response):
+        return response
+
+    data = request.json()
+    sha256 = data.get('sha256')
+    cron = data.get('cron')
+
+    if not corect_cron(cron, response):
+        return response
+
+    if not (try_add_job(sha256, cron, scheduler, response)):
+        return response
+
+    try:
+        scheduler.remove_job(job_id=sha256)
+    except Exception:
+        pass
+
+
+    scheduler.add_job(func=lambda: vt_request.request_to_vt(sha256),
+                      trigger='cron', id='control_job',
+                      replace_existing=True, **cron)
+
+    logging.info("Cron for " + sha256 + ".html has beed changed.")
+
+    # tworzymy response
+    response.status = "202 Accepted"
+    response.body = {
+        "message": "Your schedule has been created"
+    }
+
+    return response
+
+
+def is_json(request, response):
     try:
         data = request.json()
     except NotJsonError as error:
@@ -17,14 +53,14 @@ def cycle_handler(request, response, scheduler):
         response.body = {
             "error": "Given body is not json"
         }
-        return response
+        return False
+    return True
 
-    sha256 = data.get('sha256')
-    cron = data.get('cron')
-
+# badamy jedynie czy ponade wartosci mieszcza sie w przedzialach
+def try_add_job(sha256, cron, scheduler, response):
     try:
         scheduler.add_job(func=lambda: vt_request.request_to_vt(sha256),
-                          trigger='cron', id=sha256,
+                          trigger='cron', id='control_job',
                           replace_existing=True, **cron)
     except Exception as e:
         logging.error("Scheduler reject job")
@@ -32,14 +68,61 @@ def cycle_handler(request, response, scheduler):
         response.body = {
             "error": "Wrong parameters. Check corn or sha256"
         }
-        return response
+        return False
+    return True
 
 
+def corect_cron(cron, response):
+    year = cron.get('year')
+    month = cron.get('month')
+    day = cron.get('day')
+    week = cron.get('week')
+    day_of_week = cron.get('day_of_week')
+    hour = cron.get('hour')
+    minute = cron.get('minute')
+    second = cron.get('second')
+    start_date = cron.get('start_date')
+    end_date = cron.get('end_date')
 
-    # tworzymy response
-    response.status = "202 Accepted"
-    response.body = {
-        "message": "Your schedule has been created"
-    }
+    if not (year is None or (int(year) > 999 and int(year) < 10000)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong year parameter"}
+        return False
 
+    if not (month is None or (int(month) > 0 and int(month) < 13)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong month parameter"}
+        return False
+
+    if not (day is None or (int(day) > 0 and int(day) < 32)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong day parameter"}
+        return False
+
+    if not (week is None or (int(week) > 0 and int(week) < 54)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong week parameter"}
+        return False
+
+    if not (hour is None or (int(hour) > -1 and int(hour) < 24)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong hour parameter"}
+        return False
+
+    if not (minute is None or (int(minute) > -1 and int(minute) < 60)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong minute parameter"}
+        return False
+
+    if not (second is None or (int(second) > -1 and int(second) < 60)):
+        wrong_cron_response(response)
+        response.body = {"error": "Wrong second parameter"}
+        return False
+
+    return True
+
+
+def wrong_cron_response(response):
+    logging.error("Wrong corn")
+    response.status = "400 Invalid time parameters"
     return response
--- test/test_cyclerequest.py
+++ test/test_cyclerequest.py
@@ -21,8 +21,14 @@ class TestCycleRequest(TestCase):
         payload = {
             "sha256": "przykladowe_sha356",
             "cron": {
+                "year" : "2017",
+                "month" : "5",
                 "day":"1",
-                "minute" : "20"
+                "week" : "4",
+                "day_of_week" : "3",
+                "hour" : "17",
+                "minute" : "20",
+                "second" : "59"
             }
         }
         response = requests.post('http://localhost:5005/api/scheduleVirusTotal',
@@ -30,3 +36,48 @@ class TestCycleRequest(TestCase):
                                  )
 
         self.assertEqual( 202, response.status_code)
+
+    def test_scheduler_reject(self):
+        # to jest ważne, nasz serwer po content-type rozroznia co jest binary a co json
+        payload = {
+            "sha256": "przykladowe_sha356",
+            "cron": {
+                "seconde": "59"
+            }
+        }
+        response = requests.post('http://localhost:5005/api/scheduleVirusTotal',
+                                    data=json.dumps(payload)
+                                    )
+
+        self.assertEqual(406, response.status_code)
+
+
+    def test_wrong_json(self):
+        payload = {
+            "sha256": "przykladowe_sha356",
+            "cron": {
+                "day": "1",
+                "minute": "20"
+            }
+        }
+        response = requests.post('http://localhost:5005/api/scheduleVirusTotal',
+                                 data = payload
+                                 )
+
+        self.assertEqual(415, response.status_code)
+
+    def test_wrong_cron_minute(self):
+        payload = {
+            "sha256": "przykladowe_sha356",
+            "cron": {
+                "day": "1",
+                "minute": "66"
+            }
+        }
+        response = requests.post('http://localhost:5005/api/scheduleVirusTotal',
+                                 data=json.dumps(payload)
+                                 )
+        json_content = response.json()
+        message = json_content.get("error")
+        self.assertEqual(400, response.status_code)
+        self.assertEqual("Wrong minute parameter", message)
