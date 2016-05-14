407dab5337ba9c87332d7ca71ff13abd3c94fdaf
mwalercz <mwalerczuk@gmail.com>
2016-05-14 22:31:45 +0200

    removed unnecessary tests, "App" class is now "Server" class

--- test/test_server.py
+++ test/test_server.py
@@ -1,10 +1,12 @@
 import json
 import os
+
+from htmlparser import is_not_found_on_vt
 from server.fileservice import Fileservice, NoSuchFile
 import hashlib
 import requests
 from unittest import TestCase
-from server import App
+from server import Server
 from server.fileservice import Fileservice
 from server.requesthandlers.single_request import create_processingg_file
 
@@ -12,23 +14,21 @@ from server.requesthandlers.single_request import create_processingg_file
 class TestApplication(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.app = App()
-        cls.app.initialize()
+        cls.app = Server()
+        cls.app.serve()
 
     @classmethod
     def tearDownClass(cls):
         cls.app.exit_gracefully()
 
-
+    # simple requests test
     def test_scheduler_request(self):
-        # to jest ważne, nasz serwer po content-type rozroznia co jest binary a co json
         payload = {"x": 5}
         response = requests.post('http://localhost:5005/test/scheduler',
                                  data=json.dumps(payload))
         self.assertEqual(response.text, '{"x": 5}')
 
     def test_binary_request(self):
-        # to jest ważne, nasz serwer po content-type rozroznia co jest binary a co json
         payload = b'heheheszki'
         response = requests.post('http://localhost:5005/test/binary',
                                  data=payload)
@@ -39,8 +39,9 @@ class TestApplication(TestCase):
         response = requests.post('http://localhost:5005/test/no-such-url')
         self.assertEqual(response.status_code, 404)
 
+    # / simple requests test
 
-# cycle request test
+    # cycle request test
     def test_volidation(self):
         payload = {
             "sha256": "przykladowe_sha356",
@@ -55,7 +56,6 @@ class TestApplication(TestCase):
         self.assertNotEqual(406, response.status_code)
 
     def test_status(self):
-        # to jest ważne, nasz serwer po content-type rozroznia co jest binary a co json
         payload = {
             "sha256": "przykladowe_sha356",
             "cron": {
@@ -76,7 +76,6 @@ class TestApplication(TestCase):
         self.assertEqual(202, response.status_code)
 
     def test_scheduler_reject(self):
-        # to jest ważne, nasz serwer po content-type rozroznia co jest binary a co json
         payload = {
             "sha256": "przykladowe_sha356",
             "cron": {
@@ -119,20 +118,20 @@ class TestApplication(TestCase):
         self.assertEqual(400, response.status_code)
         self.assertEqual("Wrong minute parameter", message)
 
-# /cycle request test
+    # /cycle request test
 
-# single request test
-    def test_status(self):
+    # single request test
+    def test_single_status(self):
         payload = b'sprawdzamy_pojedyncze_zapytanie'
         response = requests.post('http://localhost:5005/api/singleVirusTotal',
-                                    data=payload)
+                                 data=payload)
         # dostalismy z powrotem jsona bo nasz serwer umie tylko wysylac jsony
         self.assertEqual(202, response.status_code)
 
     def test_sha256(self):
         payload = b'sprawdzamy_pojedyncze_zapytanie'
         response = requests.post('http://localhost:5005/api/singleVirusTotal',
-                                    data=payload)
+                                 data=payload)
         data = response.json()
         responseSha256 = data.get('sha256')
         expectedShaObcjet = hashlib.sha256(payload)
@@ -146,60 +145,61 @@ class TestApplication(TestCase):
             self.assertEqual(file_content, "PROCESSING")
             file.write("It shouldn't exist")
             file.remove()
-# /single request test
+    # /single request test
 
-# test virus_info request
+    #  virus_info request test
     def test_simple_attributes(self):
         payload = {'sha256': 'found',
-                    'attributes': ['MIMEType', 'XMPToolkit', 'Producer']}
+                   'attributes': ['MIMEType', 'XMPToolkit', 'Producer']}
         response = requests.post('http://localhost:5005/api/virus',
-                                             data=json.dumps(payload))
+                                 data=json.dumps(payload))
         self.assertEqual(200, response.status_code)
 
     def test_no_attributes(self):
         payload = {'sha256': 'found'}
         response = requests.post('http://localhost:5005/api/virus',
-                                        data=json.dumps(payload))
+                                 data=json.dumps(payload))
         self.assertEqual(200, response.status_code)
 
     def test_bad_json_syntax(self):
         payload = "{\"sha256\": \"found\" ,}"  # przecinek na końcu!
         response = requests.post('http://localhost:5005/api/virus',
-                                    data=payload)
+                                 data=payload)
         self.assertEqual(415, response.status_code)
 
     def test_invalid_arguments_attributes(self):
         payload = {'sha256': 'found',
-                    "ATTRIBUTES": ["MIMEType"]}
+                   "ATTRIBUTES": ["MIMEType"]}
         response = requests.post('http://localhost:5005/api/virus',
-                                    data=json.dumps(payload))
+                                 data=json.dumps(payload))
         self.assertEqual(400, response.status_code)
 
     def test_invalid_arguments_sha256(self):
         payload = {'SHA256': 'found'}
         response = requests.post('http://localhost:5005/api/virus',
-                                    data=json.dumps(payload))
+                                 data=json.dumps(payload))
         self.assertEqual(400, response.status_code)
 
     def test_not_list_in_attributes(self):
         payload = {'sha256': 'found',
-                    "attributes": "MIMEType"}
+                   "attributes": "MIMEType"}
         response = requests.post('http://localhost:5005/api/virus',
-                                    data=json.dumps(payload))
+                                 data=json.dumps(payload))
         self.assertEqual(400, response.status_code)
 
-    def test_not_found(self):
+    def test_not_exists(self):
         payload = {'sha256': 'not-exists'}
         response = requests.post('http://localhost:5005/api/virus',
-                                    data=json.dumps(payload))
+                                 data=json.dumps(payload))
         self.assertEqual(404, response.status_code)
-# /test virus_info request
 
-# test VT request
+    # / virus_info request test
+
+    # VT request test
     def test_not_found(self):
         payload = b'nieznany_plik'
         response = requests.post('http://localhost:5005/api/singleVirusTotal',
-                                    data=payload)
+                                 data=payload)
         data = response.json()
         sha256 = data.get('sha256')
         with Fileservice.File(sha256) as file:
@@ -207,9 +207,9 @@ class TestApplication(TestCase):
             self.assertEqual(file_content, "PROCESSING")
             file.remove()
 
-# /test VT request
+    # /VT request test
 
-# test Fileservice
+    # Fileservice test
 
     def test_read_wrong_file(self):
         test_sha265 = "4321"
@@ -220,7 +220,6 @@ class TestApplication(TestCase):
             with Fileservice.File(test_sha265) as file:
                 file.read()
 
-
     def test_read_write(self):
         test_string = "Omijając kwieciste ostrowy burzanu"
         test_sha256 = "1234"
@@ -229,12 +228,10 @@ class TestApplication(TestCase):
             file_content = file.read()
         self.assertEqual(file_content, test_string)
 
-
     def test_exists(self):
         with Fileservice.File("file-doesnt-exist") as file:
             if file.exists():
                 self.assertTrue(False, "nigdy nie powinnismy tutaj wejsc")
-
 
     def test_remove(self):
         with Fileservice.File("remove") as file:
@@ -243,4 +240,22 @@ class TestApplication(TestCase):
             file.remove()
             self.assertFalse(file.exists())
 
-    # /test Fileservice
+    # /Fileservice test
+
+    # Htmlparser test
+
+    def test_is_not_found_on_vt(self):
+        with Fileservice.File("not_found_on_vt") as file:
+            file_content = file.read()
+
+        bool = is_not_found_on_vt(file_content)
+        self.assertTrue(bool)
+
+    def test_is_found_on_vt(self):
+        with Fileservice.File("found") as file:
+            file_content = file.read()
+
+        bool = is_not_found_on_vt(file_content)
+        self.assertFalse(bool)
+
+    # /Htmlparser test
